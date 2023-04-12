11af002731a78024bbd37579d1536f00d9ef3ff9
Alec Wixom <atwixom@gmail.com>
2023-04-12 13:20:33 -0600

    first commit

---
+++ md1-demo.js
@@ -0,0 +1,54 @@
+let jonSnowAttack = 25
+let jamieLannisterAttack = 35
+
+if (jonSnowAttack > jamieLannisterAttack){
+    console.log("Jon has better attack than Jamie");
+} else if(jamieLannisterAttack > jonSnowAttack){
+    console.log("Jamie has better attack than Jon");
+} else {
+    console.log("Jon and Jamie have the same attack");
+}
+
+let jonSnowHealth = 100
+let jonSnowDefense = 0
+
+if (jonSnowHealth <= jamieLannisterAttack){
+    console.log("Jon has been slain");
+} else {
+    jonSnowHealth -= jamieLannisterAttack
+    console.log("Jon's health is down to " + jonSnowHealth);
+}
+
+
+console.log("============");
+
+let coinLandsHead = false
+
+if (coinLandsHead === true){
+    console.log("The fight continues");
+} else {
+    console.log("Jon is allowed to run away");
+}
+
+console.log("===========");
+
+// for( let i = 0; i < 5; i++){
+//     if(jonSnowHealth <= 0){
+//         console.log("Jon has been slain");
+//     } else {
+//         jonSnowHealth -= jamieLannisterAttack
+//         console.log(`Jon's health is ${jonSnowHealth}`);
+//         // console.log("Jon's health is " + jonSnowHealth)
+//     }
+// }
+
+while(jonSnowHealth > 0){
+    jonSnowHealth = jonSnowHealth - jamieLannisterAttack
+    if(jonSnowHealth < 0){
+        jonSnowHealth = 0
+    }
+    console.log(`Jon's health is ${jonSnowHealth}`)
+    if(jonSnowHealth <= 0){
+        console.log("Jon has been slain");
+    }
+}
